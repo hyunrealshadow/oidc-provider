6ee8280023b1d62870f68338d773f5dbc9bb92b3
UnrealShadow <unrealshadow@outlook.com>
2023-08-26 00:36:53 +0800

    refactor: Update i18next template

--- test/helpers/template.test.js
+++ test/helpers/template.test.js
@@ -11,13 +11,13 @@ describe('template helper', () => {
   });
   it('parse template string to get template information with intl format array', () => {
     const result = template`Test ${[['A1', 'A2', 'A3'], { type: 'conjunction' }]} Test ${[['B1', 'B2', 'B3'], { type: 'disjunction' }]}`;
-    expect(result.template).to.eq('Test {{key0, intlList(type: \'conjunction\')}} Test {{key1, intlList(type: \'disjunction\')}}');
+    expect(result.template).to.eq('Test {{key0, intlList(type: conjunction)}} Test {{key1, intlList(type: disjunction)}}');
     expect(result.variables).to.eql({ key0: ['A1', 'A2', 'A3'], key1: ['B1', 'B2', 'B3'] });
     expect(result.toString()).to.eq('Test \'A1\', \'A2\', and \'A3\' Test \'B1\', \'B2\', or \'B3\'');
   });
   it('parse template string to get template information with intl format array and pluralize', () => {
     const result = template`Test ${[['param', 'params', 1], { type: 'pluralize' }]} Test ${[['param', 'params', 2], { type: 'pluralize' }]}`;
-    expect(result.template).to.eq('Test {{key0, intlPluralize(one: \'param\', many: \'params\')}} Test {{key1, intlPluralize(one: \'param\', many: \'params\')}}');
+    expect(result.template).to.eq('Test {{key0, intlPluralize(one: param, many: params)}} Test {{key1, intlPluralize(one: param, many: params)}}');
     expect(result.variables).to.eql({ key0: 1, key1: 2 });
     expect(result.toString()).to.eq('Test param Test params');
   });
